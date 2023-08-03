ecdfa87ef89e47e5df12a6f4415248895be842cd
Mark Miranda <mmiranda@doximity.com>
2023-08-03 13:11:46 -0600

    Update mailer specs

--- spec/mailers/user_spec.rb
+++ spec/mailers/user_spec.rb
@@ -2,16 +2,17 @@ require "rails_helper"
 
 RSpec.describe UserMailer, type: :mailer do
   describe "welcome" do
-    let(:mail) { UserMailer.welcome }
+    let(:membership) { create(:membership) }
+    let(:mail) { UserMailer.welcome(membership) }
 
     it "renders the headers" do
       expect(mail.subject).to eq("Welcome")
-      expect(mail.to).to eq(["to@example.org"])
-      expect(mail.from).to eq(["from@example.com"])
+      expect(mail.to).to eq([membership.user.email])
+      expect(mail.from).to eq(["info@pokerleague.app"])
     end
 
     it "renders the body" do
-      expect(mail.body.encoded).to match("Hi")
+      expect(mail.body.encoded).to include("Hey friend")
     end
   end
 
